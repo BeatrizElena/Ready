e69704d043d16dda2f45851695aeef9a6de987ab
beatrizelena <ortizwebdevelopment@gmail.com>
2018-07-27 23:55:35 -0400

    Add $('.doctor-wrapper').hide() to document.ready in index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,6 +4,7 @@ const authUsers = require('./auth/events')
 const sessionEvents = require('./ready-interpreter/events')
 
 $(() => {
+  $('.doctor-wrapper').hide()
   authUsers.addHandlers()
   sessionEvents.addHandlers()
 })
